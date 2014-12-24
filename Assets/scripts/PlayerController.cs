4a0592b6a7ea212baf2ade257bc887884fbce792
Jeff Conrad <ledlogic@gmail.com>
2014-12-23 21:52:28 -0600

    Initial checkin

---
+++ Assets/scripts/PlayerController.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	public float speed;
+	private int count;
+	public GUIText countText;
+	public GUIText winText;
+
+	void Start() {
+		count = 0;
+		winText.gameObject.SetActive (false);
+		updateCountText();
+	}
+
+	void FixedUpdate() {
+		float moveHorizontal = Input.GetAxis("Horizontal");
+		float moveVertical = Input.GetAxis("Vertical");
+		Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+		rigidbody.AddForce(movement * speed * Time.deltaTime);
+	}
+
+	void OnTriggerEnter(Collider other) {
+		if (other.gameObject.tag == "Pickup") {
+			other.gameObject.SetActive(false);
+			count++;
+			updateCountText();
+
+			if (count >= 16) {
+				winText.gameObject.SetActive(true);
+			}
+		}
+	}
+
+	void updateCountText() {
+		countText.text = "Count: " + count.ToString ();
+	}
+}
---
+++ Assets/scripts/Rotator.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotator : MonoBehaviour {
+
+	private Vector3 spin;
+
+	void Start() {
+		spin = new Vector3 (15, 30, 45);
+	}
+
+	void Update () {
+		transform.Rotate(spin * Time.deltaTime);
+	}
+}
